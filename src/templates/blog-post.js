0c3bda326fb26eeb4dd30b8004b8e5b0bf1793f0
for-alisia <for-alisia@yandex.ru>
2021-02-10 16:09:21 +0500

    Added styling for single page

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,21 +1,66 @@
+/** Libraries */
 import React from "react"
 import { graphql } from "gatsby"
-import Layout from "../components/layout"
 import Img from "gatsby-image"
+import styled from "styled-components"
+
+/** Components */
+import Layout from "../components/layout"
+
+/** Styles */
+const ContentWrapperStyled = styled.div`
+  display: flex;
+  margin-top: 8rem;
+  margin-bottom: 8rem;
+
+  h1 {
+    font-size: 3.3rem;
+    max-width: 50%;
+    text-align: right;
+    padding-right: 3rem;
+    line-height: 4.5rem;
+  }
+`
+
+const ImgWrapperStyled = styled.div`
+  width: 50%;
+  position: relative;
+
+  &:before {
+    content: "";
+    display: block;
+    position: absolute;
+    top: -27px;
+    left: -27px;
+    z-index: -1;
+    width: 100%;
+    height: 100%;
+    border: 1px solid #151515;
+  }
+
+  .gatsby-image-wrapper {
+    max-height: 400px;
+  }
+`
+
+const HtmlWrapperStyled = styled.div``
 
 export default ({ data }) => {
   const post = data.markdownRemark
-  console.log(data)
 
   let featuredImgFluid = post.frontmatter.featuredImage.childImageSharp.fluid
 
   return (
     <Layout>
-      <div>
+      <ContentWrapperStyled>
         <h1>{post.frontmatter.title}</h1>
-        <Img fluid={featuredImgFluid} />
-        <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
-      </div>
+        <ImgWrapperStyled>
+          <Img fluid={featuredImgFluid} />
+        </ImgWrapperStyled>
+      </ContentWrapperStyled>
+      <HtmlWrapperStyled
+        dangerouslySetInnerHTML={{ __html: post.html }}
+      ></HtmlWrapperStyled>
     </Layout>
   )
 }
